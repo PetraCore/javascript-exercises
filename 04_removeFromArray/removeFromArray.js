eb95c2d6c2bbaff70564525d8ba897d2c2a8c06f
PetraCore <102828971+PetraCore@users.noreply.github.com>
2024-01-08 18:19:20 +0100

    Solve 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...elements) {
+    const newArray = [];
 
+    itemSearch:
+    for (let item of array) {
+        for (let element of elements) {
+            if (item === element) {
+                continue itemSearch;
+            }
+        }
+        newArray.push(item);
+    }
+
+    return newArray;
 };
 
 // Do not edit below this line
