e37d424712f7bba549237e0c18ef4fe735a63393
PetraCore <102828971+PetraCore@users.noreply.github.com>
2024-01-08 18:25:32 +0100

    Refactor 04_removeFromArray solution

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,17 +1,5 @@
-const removeFromArray = function(array, ...elements) {
-    const newArray = [];
-
-    itemSearch:
-    for (let item of array) {
-        for (let element of elements) {
-            if (item === element) {
-                continue itemSearch;
-            }
-        }
-        newArray.push(item);
-    }
-
-    return newArray;
+const removeFromArray = function(array, ...args) {
+    return array.filter((item) => !args.includes(item)); 
 };
 
 // Do not edit below this line
