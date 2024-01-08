c23bbdd84e7ef9643ca4e434f1280dfdafc39d02
PetraCore <102828971+PetraCore@users.noreply.github.com>
2024-01-08 22:13:38 +0100

    Solve 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(firstNum, lastNum) {
 
+    if (typeof firstNum !== 'number' || typeof lastNum !== 'number'
+        || firstNum < 0 || lastNum < 0) {
+        return 'ERROR';
+    }
+
+    if (firstNum > lastNum) {
+        // swap values
+        [firstNum, lastNum] = [lastNum, firstNum];
+    }
+    
+    let sum = 0;
+    for (let i = firstNum; i <= lastNum; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
