6fe42561b16d15884f34beae2486a8cfc18a4ac9
PetraCore <102828971+PetraCore@users.noreply.github.com>
2024-01-19 17:17:25 +0100

    Adjust 12_findTheOldest solution
    Old solution would modify original objects which is not a good practice

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,18 +1,15 @@
 const findTheOldest = function(people) {
     return people.reduce((oldest, person) => {
-        if (!oldest.yearOfDeath) {
-            oldest.yearOfDeath = new Date().getFullYear();
-        }
-        if (!person.yearOfDeath) {
-            person.yearOfDeath = new Date().getFullYear();
-        }
-
-        let oldestAge = oldest.yearOfDeath - oldest.yearOfBirth;
-        let personAge = person.yearOfDeath - person.yearOfBirth;
+        let oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+        let personAge = getAge(person.yearOfBirth, person.yearOfDeath);
 
         return personAge > oldestAge ? person : oldest;
     });
 };
+
+const getAge = function(birth, death = new Date().getFullYear()) {
+    return death - birth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
