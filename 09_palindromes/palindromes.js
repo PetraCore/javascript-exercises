9127a89d3bd5ec420172b643b478729927343845
PetraCore <102828971+PetraCore@users.noreply.github.com>
2024-01-19 16:22:35 +0100

    Solve 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function(str) {
+    let characters = str.toLowerCase().split('').filter((char) => char.match(/\w|\d/));
+    return characters.join('') === characters.reverse().join('');
 };
 
 // Do not edit below this line
