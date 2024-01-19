8b6458888393b650b7b42c63821fb6be4be6d41f
PetraCore <102828971+PetraCore@users.noreply.github.com>
2024-01-19 17:05:34 +0100

    Solve 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    return people.reduce((oldest, person) => {
+        if (!oldest.yearOfDeath) {
+            oldest.yearOfDeath = new Date().getFullYear();
+        }
+        if (!person.yearOfDeath) {
+            person.yearOfDeath = new Date().getFullYear();
+        }
 
+        let oldestAge = oldest.yearOfDeath - oldest.yearOfBirth;
+        let personAge = person.yearOfDeath - person.yearOfBirth;
+
+        return personAge > oldestAge ? person : oldest;
+    });
 };
 
 // Do not edit below this line
